f23e489bd68b8fbcecf4d49a1ba7e31907d82405
Vignesh Ramachandran <ramacha7@ecegrid-thin4.ecn.purdue.edu>
2019-10-30 18:48:04 -0400

    False push to solve errro

--- main.cpp
+++ main.cpp
@@ -9,38 +9,38 @@ using namespace std;
 int main(int argc, char** argv)
 {
 	//ifstream file("interpreter_input.smp", ios::binary);
-	ifstream file(argv[1], ios::binary);
+	// ifstream file(argv[1], ios::binary);
 
-	file.unsetf(ios::skipws);
-	streampos fileSize;
-	file.seekg(0, ios::end);
-	fileSize = file.tellg();
-	file.seekg(0, ios::beg);
+	// file.unsetf(ios::skipws);
+	// streampos fileSize;
+	// file.seekg(0, ios::end);
+	// fileSize = file.tellg();
+	// file.seekg(0, ios::beg);
 
-	cout << "size:" << fileSize << endl;
+	// cout << "size:" << fileSize << endl;
 
 	vector<unsigned char> mem;
 	
-	mem.reserve(fileSize);
+	// mem.reserve(fileSize);
 
-	mem.insert(mem.begin(), istreambuf_iterator<char>(file), istreambuf_iterator<char>());
+	// mem.insert(mem.begin(), istreambuf_iterator<char>(file), istreambuf_iterator<char>());
 
-	file.close();
+	// file.close();
 
-	// mem.reserve(11);
-	// mem.push_back((unsigned char) 70);
-	// mem.push_back((unsigned char) 16);
-	// mem.push_back((unsigned char) 0);
-	// mem.push_back((unsigned char) 0);
-	// mem.push_back((unsigned char) 0);
-	// mem.push_back((unsigned char) 70);
-	// mem.push_back((unsigned char) 17);
-	// mem.push_back((unsigned char) 0);
-	// mem.push_back((unsigned char) 0);
-	// mem.push_back((unsigned char) 0);
-	// mem.push_back((unsigned char) 140);
+	mem.reserve(11);
+	mem.push_back((unsigned char) 70);
+	mem.push_back((unsigned char) 16);
+	mem.push_back((unsigned char) 0);
+	mem.push_back((unsigned char) 0);
+	mem.push_back((unsigned char) 0);
+	mem.push_back((unsigned char) 70);
+	mem.push_back((unsigned char) 17);
+	mem.push_back((unsigned char) 0);
+	mem.push_back((unsigned char) 0);
+	mem.push_back((unsigned char) 0);
+	mem.push_back((unsigned char) 140);
 
-	for (int i = 0; i < fileSize; ++i)
+	for (int i = 0; i < 11; ++i)
 	{
 		cout << +mem[i] << endl;
 	}
