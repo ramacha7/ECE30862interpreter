0898639771546f61f47e21345e6793f4c60e27c9
Natalie Rodeghier <42589066+nrodeghi@users.noreply.github.com>
2019-11-03 15:21:39 -0500

    final?

--- main.cpp
+++ main.cpp
@@ -9,8 +9,8 @@ using namespace std;
 int main(int argc, char** argv)
 {
 	//ifstream file("interpreter_input.smp", ios::binary);
-	ifstream file("Jumps.bin", ios::binary);
-	//ifstream file(argv[1], ios::binary);
+	//ifstream file("Subroutines.bin", ios::binary);
+	ifstream file(argv[1], ios::binary);
 
 	file.unsetf(ios::skipws);
 	streampos fileSize;
@@ -18,7 +18,7 @@ int main(int argc, char** argv)
 	fileSize = file.tellg();
 	file.seekg(0, ios::beg);
 
-	cout << "size:" << fileSize << endl;
+	//cout << "size:" << fileSize << endl;
 
 	vector<unsigned char> mem;
 	
